b33696dc5fbf07ded33502eed8d159395b16093b
Jim Drury <jim@jimdrury.co.uk>
2024-02-09 17:34:07 +0000

    feat: adds resolve relations and resolve links to bridge

--- projects/geometricpanda/ng-storyblok/render/root/storyblok-root.component.ts
+++ projects/geometricpanda/ng-storyblok/render/root/storyblok-root.component.ts
@@ -1,6 +1,10 @@
 import { AsyncPipe, isPlatformBrowser } from '@angular/common';
 import { Component, PLATFORM_ID, computed, effect, inject, input, signal } from '@angular/core';
-import { NG_STORYBLOK_BRIDGE, NG_STORYBLOK_CLIENT } from '@geometricpanda/ng-storyblok/tokens';
+import {
+    NG_STORYBLOK_BRIDGE,
+    NG_STORYBLOK_RESOLVE_LINKS,
+    NG_STORYBLOK_RESOLVE_RELATIONS,
+} from '@geometricpanda/ng-storyblok/tokens';
 import { ISbStory, useStoryblokBridge } from '@storyblok/js';
 import { ISbStoryData } from 'storyblok-js-client/src/interfaces';
 import { StoryblokContentDirective } from '../render';
@@ -14,7 +18,8 @@ import { StoryblokContentDirective } from '../render';
 export class StoryblokRootComponent {
     PLATFORM_ID = inject(PLATFORM_ID);
     BRIDGE = inject(NG_STORYBLOK_BRIDGE, { optional: true });
-    API = inject(NG_STORYBLOK_CLIENT);
+    RESOLVE_RELATIONS = inject(NG_STORYBLOK_RESOLVE_RELATIONS, { optional: true }) ?? undefined;
+    RESOLVE_LINKS = inject(NG_STORYBLOK_RESOLVE_LINKS, { optional: true }) ?? undefined;
 
     story = input.required<ISbStory>();
     bridgeStory = signal<ISbStoryData | undefined>(undefined);
@@ -37,7 +42,10 @@ export class StoryblokRootComponent {
             const platformBrowser = isPlatformBrowser(this.PLATFORM_ID);
 
             if (platformBrowser && bridge) {
-                useStoryblokBridge(story.data.story.id, (newStory) => this.bridgeStory.set(newStory));
+                useStoryblokBridge(story.data.story.id, (newStory) => this.bridgeStory.set(newStory), {
+                    resolveRelations: this.RESOLVE_RELATIONS,
+                    resolveLinks: this.RESOLVE_LINKS,
+                });
             }
         },
         { allowSignalWrites: true },
