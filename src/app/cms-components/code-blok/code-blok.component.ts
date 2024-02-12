68e5b0a9fb299e2ae6dbaf18a1439c92f42d0c0a
Jim Drury <jim.drury@virginmediao2.co.uk>
2024-02-12 09:50:51 +0000

    feat: adds visual indicator to code-blok on copy

--- src/app/cms-components/code-blok/code-blok.component.ts
+++ src/app/cms-components/code-blok/code-blok.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, ViewEncapsulation } from '@angular/core';
+import { Component, computed, effect, inject, input, signal, ViewEncapsulation } from '@angular/core';
 import { StoryblokBlok } from '@geometricpanda/ng-storyblok';
 
 import hljs from 'highlight.js/lib/core';
@@ -8,6 +8,8 @@ import css from 'highlight.js/lib/languages/css';
 import typescript from 'highlight.js/lib/languages/typescript';
 import xml from 'highlight.js/lib/languages/xml';
 
+import { animate, style, transition, trigger } from '@angular/animations';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { CodeBlok } from './code-blok.interface';
 
 hljs.registerLanguage('ts', typescript);
@@ -21,12 +23,40 @@ hljs.registerLanguage('html', xml);
     styleUrl: './code-blok.component.css',
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    host: {
-        // ['[class.code-blok]']: 'true',
-    },
+    animations: [
+        trigger('fade', [
+            transition('void => *', [style({ opacity: 0 }), animate(300, style({ opacity: 1 }))]),
+            transition('* => void', [animate(300, style({ opacity: 0 }))]),
+        ]),
+    ],
 })
 export class CodeBlokComponent implements StoryblokBlok<CodeBlok> {
+    liveAnnouncer = inject(LiveAnnouncer);
+
     blok = input.required<CodeBlok>();
+
+    copied = signal(false);
+    copiedError = signal(false);
+
+    onCopied = effect(
+        () => {
+            const copied = this.copied();
+            if (copied) {
+                setTimeout(() => this.copied.set(false), 1000);
+            }
+        },
+        { allowSignalWrites: true },
+    );
+
+    onCopiedError = effect(
+        () => {
+            const copiedError = this.copiedError();
+            if (copiedError) {
+                setTimeout(() => this.copiedError.set(false), 1500);
+            }
+        },
+        { allowSignalWrites: true },
+    );
 
     code = computed(() => {
         const blok = this.blok();
@@ -37,6 +67,13 @@ export class CodeBlokComponent implements StoryblokBlok<CodeBlok> {
     async copy() {
         const blok = this.blok();
         const { code } = blok.code;
-        await navigator.clipboard.writeText(code);
+        try {
+            await navigator.clipboard.writeText(code);
+            this.copied.set(true);
+            await this.liveAnnouncer.announce('Code copied to clipboard', 'assertive');
+        } catch (error) {
+            this.copiedError.set(true);
+            await this.liveAnnouncer.announce('There was en error copying to your clipboard', 'assertive');
+        }
     }
 }
