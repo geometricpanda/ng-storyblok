89f571a881ed7e0c64534fdc05d6a59bfb9a81a8
Jim Drury <jim@jimdrury.co.uk>
2024-02-13 08:46:54 +0000

    fix: code-blok bug when user smashes copy button rapidly

--- src/app/cms-components/code-blok/code-blok.component.ts
+++ src/app/cms-components/code-blok/code-blok.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, inject, input, signal, ViewEncapsulation } from '@angular/core';
+import { Component, computed, inject, input, signal, ViewEncapsulation } from '@angular/core';
 import { StoryblokBlok } from '@geometricpanda/ng-storyblok';
 
 import hljs from 'highlight.js/lib/core';
@@ -10,12 +10,20 @@ import xml from 'highlight.js/lib/languages/xml';
 
 import { animate, style, transition, trigger } from '@angular/animations';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { catchError, delay, noop, Subject, switchMap, tap, throttleTime } from 'rxjs';
 import { CodeBlok } from './code-blok.interface';
 
 hljs.registerLanguage('ts', typescript);
 hljs.registerLanguage('sh', bash);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('html', xml);
+
+enum COPIED_STATE {
+    HIDE = 'HIDE',
+    COPIED = 'COPIED',
+    ERROR = 'ERROR',
+}
 
 @Component({
     selector: 'app-code-blok',
@@ -25,8 +33,8 @@ hljs.registerLanguage('html', xml);
     standalone: true,
     animations: [
         trigger('fade', [
-            transition('void => *', [style({ opacity: 0 }), animate(300, style({ opacity: 1 }))]),
-            transition('* => void', [animate(300, style({ opacity: 0 }))]),
+            transition('void => *', [style({ opacity: 0 }), animate(150, style({ opacity: 1 }))]),
+            transition('* => void', [animate(150, style({ opacity: 0 }))]),
         ]),
     ],
 })
@@ -35,28 +43,31 @@ export class CodeBlokComponent implements StoryblokBlok<CodeBlok> {
 
     blok = input.required<CodeBlok>();
 
-    copied = signal(false);
-    copiedError = signal(false);
+    copied = signal<COPIED_STATE>(COPIED_STATE.HIDE);
 
-    onCopied = effect(
-        () => {
-            const copied = this.copied();
-            if (copied) {
-                setTimeout(() => this.copied.set(false), 1000);
-            }
-        },
-        { allowSignalWrites: true },
-    );
+    COPIED_STATE = COPIED_STATE;
 
-    onCopiedError = effect(
-        () => {
-            const copiedError = this.copiedError();
-            if (copiedError) {
-                setTimeout(() => this.copiedError.set(false), 1500);
-            }
-        },
-        { allowSignalWrites: true },
-    );
+    doCopy = new Subject<void>();
+    doCopyAction = this.doCopy
+        .pipe(
+            takeUntilDestroyed(),
+            throttleTime(1000),
+            switchMap(() => {
+                const blok = this.blok();
+                return navigator.clipboard.writeText(blok.code.code);
+            }),
+            switchMap(() => {
+                this.copied.set(COPIED_STATE.COPIED);
+                return this.liveAnnouncer.announce('Code copied to clipboard', 'assertive');
+            }),
+            catchError(() => {
+                this.copied.set(COPIED_STATE.ERROR);
+                return this.liveAnnouncer.announce('There was en error copying to your clipboard', 'assertive');
+            }),
+            delay(800),
+            tap(() => this.copied.set(COPIED_STATE.HIDE)),
+        )
+        .subscribe(noop);
 
     code = computed(() => {
         const blok = this.blok();
@@ -64,16 +75,7 @@ export class CodeBlokComponent implements StoryblokBlok<CodeBlok> {
         return hljs.highlightAuto(code, [language || '']).value;
     });
 
-    async copy() {
-        const blok = this.blok();
-        const { code } = blok.code;
-        try {
-            await navigator.clipboard.writeText(code);
-            this.copied.set(true);
-            await this.liveAnnouncer.announce('Code copied to clipboard', 'assertive');
-        } catch (error) {
-            this.copiedError.set(true);
-            await this.liveAnnouncer.announce('There was en error copying to your clipboard', 'assertive');
-        }
+    copy() {
+        this.doCopy.next();
     }
 }
